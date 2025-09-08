b55cea2dd7998b9eed7355ccd629639e44423766
chonsy <beatbox1871@gmail.com>
2025-09-08 16:48:10 +0800

    Update GitHub Actions workflow to use master branch for deployment

--- pro/vite.config.js
+++ pro/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Vinque_final/',
   server: {
     port: 5177,
     proxy: {
